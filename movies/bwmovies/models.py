ebc8a883f3b1ee488112d587c22e14cb142e9233
jingjun <254032134@qq.com>
2020-07-21 00:50:12 +0800

    fix: 创建项目，迁移数据库

---
+++ movies/bwmovies/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class dongzuopian(models.Model):
+    movie_type = models.CharField(max_length=30)
+    movie_name = models.CharField(max_length=100)
+    movie_star = models.IntegerField()
+    movie_actor = models.CharField(max_length=100)
+    movie_url = models.CharField(max_length=100)
+    movie_img = models.CharField(max_length=100)
